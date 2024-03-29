8bd5ec0382a49327bbae4397157832b57311ff69
rhuang4F2023 <rhuang4@andrew.cmu.edu>
2024-03-28 19:27:31 -0700

    move navbar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Drawer, Box, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BuildIcon from '@mui/icons-material/Build';
+import EventIcon from '@mui/icons-material/Event';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import logo from '../assets/logo.PNG';
+
+const Navbar = ({ isDrawerOpen, handleDrawerToggle }) => {
+  return (
+    <>
+      <Drawer
+        anchor="right"
+        variant="temporary"
+        open={isDrawerOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{ keepMounted: true }}
+        sx={{
+          '& .MuiDrawer-paper': { width: 250, boxSizing: 'border-box' },
+        }}
+      >
+        <Box sx={{ width: 250 }} role="presentation">
+          <img src={logo} alt="ResidentMe Logo" style={{ maxWidth: '150px', margin: '16px' }} />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Communication Board" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <ShoppingCartIcon />
+              </ListItemIcon>
+              <ListItemText primary="Second Hand Marketplace" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <BuildIcon />
+              </ListItemIcon>
+              <ListItemText primary="Work Order" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <EventIcon />
+              </ListItemIcon>
+              <ListItemText primary="Booking" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <NotificationsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Notifications" />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Grid, Typography, Button, Drawer, Box, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import BuildIcon from '@mui/icons-material/Build';
-import EventIcon from '@mui/icons-material/Event';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Paper from '@mui/material/Paper';
-import styled from '@mui/material/styles/styled';
-import logo from '../assets/logo.PNG';
+import Navbar from '../components/NavBar';
 
 const HomePage = () => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
@@ -24,53 +17,7 @@ const HomePage = () => {
           <MenuIcon />
         </IconButton>
       </Box>
-      <Drawer
-        anchor="right"
-        variant="temporary"
-        open={isDrawerOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{ keepMounted: true }}
-        sx={{
-          '& .MuiDrawer-paper': { width: 250, boxSizing: 'border-box' },
-        }}
-      >
-        <Box sx={{ width: 250 }} role="presentation">
-          <img src={logo} alt="ResidentMe Logo" style={{ maxWidth: '150px', margin: '16px' }} />
-          <List>
-            <ListItem button>
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText primary="Communication Board" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <ShoppingCartIcon />
-              </ListItemIcon>
-              <ListItemText primary="Second Hand Marketplace" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <BuildIcon />
-              </ListItemIcon>
-              <ListItemText primary="Work Order" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <EventIcon />
-              </ListItemIcon>
-              <ListItemText primary="Booking" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <NotificationsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Notifications" />
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
-
+      <Navbar isDrawerOpen={isDrawerOpen} handleDrawerToggle={handleDrawerToggle} />
       <Grid container spacing={3} justifyContent="center">
         <Grid item xs={12} md={8}>
           <Typography variant="h4" component="h1" gutterBottom>
